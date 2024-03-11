afda5b8afffceb6e0153a6bb739969750e85042a
feryalmoneer <fryaljaber12@gmail.com>
2024-03-11 19:47:00 +0200

    react

--- src/Item/Component/Item.jsx
+++ src/Item/Component/Item.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
 import { Zoom, toast } from "react-toastify";
-
+import './Item.css'
 export default function Item() {
 const[item ,setItem]= useState([]);
   const{id} = useParams();
@@ -35,5 +35,36 @@ const[item ,setItem]= useState([]);
   }, []);
 return (
   <>
+ <div className="container">
+<header>
+  <div className=" shrikhand-regular ">Productds</div>
+  <div className="icon_cart">
+  <svg  aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.3L19 7H7.3" />
+</svg>
+<span>0</span>
+</div>
+</header>
+
+  <div className="item_product">
+  {item.map(it =>
+
+<div className="item" key={it._id}>
+<img src ={it.mainImage.secure_url} alt="item-img"/>
+<h2> {it.name} </h2>
+<div className="price">${it.price}</div>
+<button className="addCart">ADD To Cart</button>
+
+</div>
+
+  )}
+
+</div>
+
+
+
+
+ </div>
+
 </>)
 }
